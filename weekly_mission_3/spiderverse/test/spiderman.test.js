bf986348a073476e7d59687dedd2fa18c2242423
Enrique Ibarram <ibramoz@hotmail.com>
2022-04-29 13:38:17 -0500

    Modificando valores para hacer fallar la prueba

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -4,7 +4,7 @@ describe("Unit Tests for Spiderman class", () => {
     test('1) Create an spiderman object', () => {
         const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
         expect(andrewGarfield.name).toBe("Spiderman Sony")
-        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.age).toBe("31")
         expect(andrewGarfield.actor).toBe("Andrew Garfield")
         expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
