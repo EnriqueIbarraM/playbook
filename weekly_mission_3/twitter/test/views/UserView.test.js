741795782e1002d54581ab1ce63ddfe1e5af0db9
Enrique Ibarram <ibramoz@hotmail.com>
2022-05-02 17:10:27 -0500

    Prueba 3 agregada

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -2,17 +2,22 @@ const UserView = require('./../../app/views/UserView')
 
 describe("Test for UserView", () => {
 
-    test("Return an error object when try to create a new user with an null payload", () => {
+    test("1. Return an error object when try to create a new user with an null payload", () => {
         const payload = null
         const result = UserView.createUser(payload)
         //https://jestjs.io/docs/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/)
     });
 
-    test("Return an error object when try to create a new user with a payload invalid properties", () => {
+    test("2. Return an error object when try to create a new user with a payload invalid properties", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
-        console.log(result.error)
+    });
+
+    test("3. Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
     });
 })
