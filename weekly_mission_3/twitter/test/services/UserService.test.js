a60658b5e3f027f848ef2ea7df0b7c770fc9d306
Enrique Ibarram <ibramoz@hotmail.com>
2022-05-01 20:28:49 -0500

    Prueba de requerimiento 2

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -9,4 +9,13 @@ describe("Test for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     });
+
+    test("2. Get info from an User object in a list", () => {
+        const user = UserService.create(1, "the2cto", "Enrique")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("the2cto")
+        expect(userInfoInList[2]).toBe("Enrique")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    });
 })
