b841821c54df3b9ccf6c3c10ed89765515099d4b
Enrique Ibarram <ibramoz@hotmail.com>
2022-05-02 15:23:18 -0500

    Agregando exportación de clase

---
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -0,0 +1,13 @@
+const UserService = require('./../services/UserService')
+
+class UserView{
+
+    static createUser(payload){
+        if(payload === null){
+            console.log("Error es null")
+            return {error: "payload no existe"}
+        } 
+    }
+}
+
+module.exports = UserView
