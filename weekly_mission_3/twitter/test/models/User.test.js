0e367aa600e9cf1ba4c70d1bfad3c88124d5f87d
Enrique Ibarram <ibramoz@hotmail.com>
2022-05-01 19:13:08 -0500

    Modificación de prueba, no envío de parámetros de fechas y verificación de datos no Undefined

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -4,7 +4,7 @@ describe("Unit Test for User Class", () => {
 
     test('Create an User object', () => {
         //Aquí se invoca el código a utilizar en la app
-        const user = new User(1, "the2cto", "Enrique", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "the2cto", "Enrique", "Bio")
 
         //Aquí se validan los resultados de ese código
         //Esta es una comparación que va a gualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -12,7 +12,7 @@ describe("Unit Test for User Class", () => {
         expect(user.username).toBe("the2cto")
         expect(user.name).toBe("Enrique")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeundefined() //Verifica que el valor no sea Undefined
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
